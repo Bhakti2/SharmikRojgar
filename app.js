ddef87c33533054c441ce381fec2440cefe0e948
Bhakti Dilkhush <bhaktidilkhush.2000@gmail.com>
2021-04-29 18:03:16 +0530

    fifth commit

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ app.controller('myCtrl',function($scope,$http){
         console.log("persons",$scope.persons);
     })
 })
+
+app.controller('jobCtrl',function($scope,$http){
+    $http.get("https://shamikrojgar.jugalkpatel.repl.co/jobs")
+    .then(function(response){
+        $scope.jobs = response.data.data;
+    })
+})

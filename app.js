81b5d8255ae7f8b227f7ed2668c942efdbba01dd
Bhakti Dilkhush <bhaktidilkhush.2000@gmail.com>
2021-05-03 18:18:26 +0530

    changes in files

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-document.querySelector('.img-btn').addEventListener('click', function() 
+document.querySelector('.img-btn').addEventListener('click', function()
 {
     document.querySelector('.cont').classList.toggle('s-signup');
 });
 
-var app = angular.module('myApp',[]);
-app.controller('myCtrl',function($scope,$http){
-    $http.get("https://shamikrojgar.jugalkpatel.repl.co/posts")
-    .then(function(response){
-        console.log(response.data);
-        $scope.persons = response.data.data;
-        console.log("persons",$scope.persons);
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/feed", {
+      templateUrl: "feed.html",
     })
-})
+    .when("/job", {
+      templateUrl: "job.html",
+    })
+    .when("/login", {
+      templateUrl: "/login.html",
+    })
+    .otherwise({
+      redirectTo: "/index.html",
+    });
+});
+app.controller("myCtrl", function ($scope, $http) {
+  $http
+    .get("https://shamikrojgar.jugalkpatel.repl.co/posts")
+    .then(function (response) {
+      console.log(response.data);
+      $scope.persons = response.data.data;
+      console.log("persons", $scope.persons);
+    });
+});
 
-app.controller('jobCtrl',function($scope,$http){
-    $http.get("https://shamikrojgar.jugalkpatel.repl.co/jobs")
-    .then(function(response){
-        $scope.jobs = response.data.data;
-    })
-})
+app.controller("jobCtrl", function ($scope, $http) {
+  $http
+    .get("https://shamikrojgar.jugalkpatel.repl.co/jobs")
+    .then(function (response) {
+      $scope.jobs = response.data.data;
+    });
+});
